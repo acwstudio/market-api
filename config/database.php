58897078728059d6a3dcbaa3a32d0d6633261cd3
Р•СЂРјРѕР»Р°РµРІ РђРЅСЊС‚РѕРЅ РЅРђРЅРґСЂРµРµРІРёС‡ <aermolaev@synergy.ru>
2021-07-22 11:03:35 +0300

    fix

--- config/database.php
+++ config/database.php
@@ -59,7 +59,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', __DIR__ . '/../database/mysql.crt'),
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA', ''),
             ]) : [],
         ],
 
