a9cdfb0887f5fc66cc01bcdeada090d20c2508b6
Р•СЂРјРѕР»Р°РµРІ РђРЅСЊС‚РѕРЅ РЅРђРЅРґСЂРµРµРІРёС‡ <aermolaev@synergy.ru>
2021-07-29 13:05:34 +0300

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -23,6 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        $scheme = parse_url(config('app.url'), PHP_URL_SCHEME);
+        URL::forceScheme($scheme);
     }
 }
