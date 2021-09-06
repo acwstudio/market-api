ca411cc01375ed71104bb26f4a019540476cc1fb
Musa Tagiev <musatagiev@yandex.ru>
2021-09-06 19:25:05 +0300

    OGM-482 added test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,69 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use App\Core\Input\Fields\Product\ProductGetList;
-use App\Core\Response\ResponseTrait;
-use App\Repositories\ProductRepository;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Symfony\Component\HttpFoundation\Response;
-use App\Models\Product;
-use App\Core\Error\ErrorManager;
+use App\Models\Page;
+use Illuminate\Database\Eloquent\Builder;
+use Spatie\QueryBuilder\AllowedFilter;
+use Spatie\QueryBuilder\QueryBuilder;
+use App\Http\Resources\Site\PageResource;
 
 class TestController extends Controller
 {
-    use ResponseTrait;
-
-    /**
-     * @var ProductRepository
-     */
-    public $productRepository;
-
-    public function __construct(ProductRepository $productRepository)
-    {
-        $this->productRepository = $productRepository;
-    }
-
     public function test(Request $request)
     {
-        if (!$request->isJson()) {
-            return $this->errorResponse([
-                ErrorManager::buildValidateError(VALIDATION_REQUEST_JSON_EXPECTED)->toArray()
-            ], Response::HTTP_UNPROCESSABLE_ENTITY);
-        }
+        $query = QueryBuilder::for(Page::class)
+            ->allowedFilters([
+                AllowedFilter::exact('id'),
+                AllowedFilter::exact('slug'),
+            ])
+            ->with(['components', 'components.methods', 'components.methods.method', 'seotags'])
+            ->firstOrFail();
 
-        $requestFieldSet = new ProductGetList($request->json()->all() ? $request->json()->all() : []);
+        $pageResource = new PageResource($query);
 
-        $requestFieldSet->validate();
+        dd($pageResource);
 
-        $errors = $requestFieldSet->getErrorsArray();
-
-        if (count($errors)) {
-            return $this->errorResponse($errors, Response::HTTP_OK);
-        }
-
-        $requestFieldSet->prepare();
-
-        $filteredResults = $this->productRepository->getList($requestFieldSet);
-
-        $list = [];
-        /** @var Product $product */
-        foreach ($filteredResults['products'] as $product) {
-
-            $list[] = [
-                Product::FIELD_ID => $product->getId(),
-                Product::FIELD_NAME => $product->getName(),
-                Product::FIELD_SLUG => $product->getSlug()
-            ];
-        }
-
-//        dd(
-//            $requestFieldSet->getFilter()->getName()->getValue(),
-//            $request->json()->all()
-//        );
-
-        return $this->successResponse([
-            'list' => $list,
-            'count' => $filteredResults['count'],
+        return response()->json([
+            'success' =>true,
+            'data' => $pageResource
         ]);
     }
 }
