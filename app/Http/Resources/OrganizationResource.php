8d40f7e502556854c80bea077acd07cddd879bf0
Musa Tagiev <musatagiev@yandex.ru>
2021-09-16 19:33:56 +0300

    OGM-710 fix

--- app/Http/Resources/OrganizationResource.php
+++ app/Http/Resources/OrganizationResource.php
@@ -10,7 +10,7 @@ class OrganizationResource extends JsonResource
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
@@ -19,26 +19,27 @@ class OrganizationResource extends JsonResource
         $organization = $this->resource;
 
         return [
-            Organization::FIELD_ID                 => $organization->getId(),
-            'type'                                 => 'organizations',
-            Organization::FIELD_PUBLISHED          => $organization->getPublished(),
-            Organization::FIELD_NAME               => $organization->getName(),
-            Organization::FIELD_ABBREVIATION_NAME  => $organization->getAbbreviationName(),
-            Organization::FIELD_SLUG               => $organization->getSlug(),
-            Organization::FIELD_LAND               => $organization->getLand(),
-            Organization::FIELD_SUBTITLE           => $organization->getSubtitle(),
-            Organization::FIELD_DESCRIPTION        => $organization->getDescription(),
-            Organization::FIELD_HTML_BODY          => $organization->getHtmlBody(),
-            Organization::FIELD_LOGO_CODE          => $organization->getLogoCode(),
-            Organization::FIELD_COLOR_CODE_TITLES  => $organization->getColorCodeTitles(),
-            Organization::FIELD_PREVIEW_IMAGE      => $organization->getPreviewImage(),
-            Organization::FIELD_DIGITAL_IMAGE      => $organization->getDigitalImage(),
-            Organization::FIELD_ADDRESS            => $organization->getAddress(),
-            Organization::FIELD_TYPE_TEXT          => $organization->getTypeText(),
-            Organization::FIELD_MAP_LINK           => $organization->getMapLink(),
-            Organization::FIELD_PARENT_ID          => $organization->getParentId(),
-            Organization::FIELD_CREATED_AT         => $organization->getCreatedAt(),
-            Organization::FIELD_UPDATED_AT         => $organization->getUpdatedAt(),
+            Organization::FIELD_ID                => $organization->getId(),
+            'type'                                => 'organizations',
+            Organization::FIELD_PUBLISHED         => $organization->getPublished(),
+            Organization::FIELD_NAME              => $organization->getName(),
+            Organization::FIELD_ABBREVIATION_NAME => $organization->getAbbreviationName(),
+            Organization::FIELD_SLUG              => $organization->getSlug(),
+            Organization::FIELD_LAND              => $organization->getLand(),
+            Organization::FIELD_SUBTITLE          => $organization->getSubtitle(),
+            Organization::FIELD_DESCRIPTION       => $organization->getDescription(),
+            Organization::FIELD_HTML_BODY         => $organization->getHtmlBody(),
+            Organization::FIELD_LOGO_CODE         => $organization->getLogoCode(),
+            Organization::FIELD_LOGO              => $organization->getDigitalImage(),
+            Organization::FIELD_COLOR_CODE_TITLES => $organization->getColorCodeTitles(),
+            Organization::FIELD_PREVIEW_IMAGE     => $organization->getPreviewImage(),
+            Organization::FIELD_DIGITAL_IMAGE     => $organization->getDigitalImage(),
+            Organization::FIELD_ADDRESS           => $organization->getAddress(),
+            Organization::FIELD_TYPE_TEXT         => $organization->getTypeText(),
+            Organization::FIELD_MAP_LINK          => $organization->getMapLink(),
+            Organization::FIELD_PARENT_ID         => $organization->getParentId(),
+            Organization::FIELD_CREATED_AT        => $organization->getCreatedAt(),
+            Organization::FIELD_UPDATED_AT        => $organization->getUpdatedAt(),
         ];
     }
 }
