e7eaac6d5fa024dac9c3d1df5f14f8adf318b0df
Andrey <uistinkatu5a17@yandex.ru>
2021-10-01 16:29:48 +0300

    OGM-746 fix SearchRepository interface

--- app/Services/Search/SearchRepository.php
+++ app/Services/Search/SearchRepository.php
@@ -3,10 +3,8 @@
 namespace App\Services\Search;
 
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
 
 interface SearchRepository
 {
-//    public function search(string $query): Collection;
     public function search(string $model, string $query): Collection;
 }
