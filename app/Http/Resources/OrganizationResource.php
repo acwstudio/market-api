36e843eb464dcde1d2ed576d87691c5997b7c3a2
DShpachenko <dshpachenko@synergy.ru>
2021-10-17 23:39:01 +0300

    OGM-948 Рефакторинг ресурса и коллекции Организации

--- app/Http/Resources/OrganizationResource.php
+++ app/Http/Resources/OrganizationResource.php
@@ -1,11 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Resources;
 
 use App\Models\Organization;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
-class OrganizationResource extends JsonResource
+final class OrganizationResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -19,28 +22,28 @@ class OrganizationResource extends JsonResource
         $organization = $this->resource;
 
         return [
-            Organization::FIELD_ID                => $organization->getId(),
+            Organization::FIELD_ID                => $organization->id,
             'type'                                => 'organizations',
-            Organization::FIELD_PUBLISHED         => $organization->getPublished(),
-            Organization::FIELD_NAME              => $organization->getName(),
-            Organization::FIELD_ABBREVIATION_NAME => $organization->getAbbreviationName(),
-            Organization::FIELD_SLUG              => $organization->getSlug(),
-            Organization::FIELD_LAND              => $organization->getLand(),
-            Organization::FIELD_SUBTITLE          => $organization->getSubtitle(),
-            Organization::FIELD_DESCRIPTION       => $organization->getDescription(),
-            Organization::FIELD_HTML_BODY         => $organization->getHtmlBody(),
-            Organization::FIELD_LOGO_CODE         => $organization->getLogoCode(),
-            Organization::FIELD_LOGO              => $organization->getPreviewImage(),
-            Organization::FIELD_COLOR_CODE_TITLES => $organization->getColorCodeTitles(),
-            Organization::FIELD_PREVIEW_IMAGE     => $organization->getPreviewImage(),
-            Organization::FIELD_DIGITAL_IMAGE     => $organization->getDigitalImage(),
-            Organization::FIELD_ADDRESS           => $organization->getAddress(),
-            Organization::FIELD_TYPE_TEXT         => $organization->getTypeText(),
-            Organization::FIELD_MAP_LINK          => $organization->getMapLink(),
-            Organization::FIELD_PARENT_ID         => $organization->getParentId(),
-            Organization::FIELD_CITY_ID           => $organization->getCityId(),
-            Organization::FIELD_CREATED_AT        => $organization->getCreatedAt(),
-            Organization::FIELD_UPDATED_AT        => $organization->getUpdatedAt(),
+            Organization::FIELD_PUBLISHED         => $organization->published,
+            Organization::FIELD_NAME              => $organization->name,
+            Organization::FIELD_ABBREVIATION_NAME => $organization->abbreviation_name,
+            Organization::FIELD_SLUG              => $organization->slug,
+            Organization::FIELD_LAND              => $organization->land,
+            Organization::FIELD_SUBTITLE          => $organization->subtitle,
+            Organization::FIELD_DESCRIPTION       => $organization->description,
+            Organization::FIELD_HTML_BODY         => $organization->html_body,
+            Organization::FIELD_LOGO_CODE         => $organization->logo_code,
+            Organization::FIELD_LOGO              => Storage::url($organization->preview_image),
+            Organization::FIELD_COLOR_CODE_TITLES => $organization->color_code_titles,
+            Organization::FIELD_PREVIEW_IMAGE     => Storage::url($organization->preview_image),
+            Organization::FIELD_DIGITAL_IMAGE     => Storage::url($organization->digital_image),
+            Organization::FIELD_ADDRESS           => $organization->address,
+            Organization::FIELD_TYPE_TEXT         => $organization->type_text,
+            Organization::FIELD_MAP_LINK          => $organization->map_link,
+            Organization::FIELD_PARENT_ID         => $organization->parent_id,
+            Organization::FIELD_CITY_ID           => $organization->city_id,
+            Organization::FIELD_CREATED_AT        => $organization->created_at,
+            Organization::FIELD_UPDATED_AT        => $organization->updated_at,
             'included'                            => [
                 Organization::ENTITY_RELATIVE_CITY    => CityResource::make($this->whenLoaded(Organization::ENTITY_RELATIVE_CITY)),
                 Organization::ENTITY_RELATIVE_PERSONS => PersonResource::collection($this->whenLoaded(Organization::ENTITY_RELATIVE_PERSONS)),
